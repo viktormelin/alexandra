20564e6c181fa757f0ca57fe50633b4b85e2923d
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-24 10:06:35 +0100

    add point counter

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,13 +1,19 @@
 import getChuckJoke from "./services/chuckService";
 import humorService from './services/humorService'
 
+let points = 0;
+
 function init() {
   addEventListeners();
 }
 
 function addEventListeners() {
   let button = document.querySelector('button') as HTMLButtonElement;
+  const addBtn = document.querySelector('#add') as HTMLButtonElement;
+  const removeBtn = document.querySelector('#remove') as HTMLButtonElement;
 
+  addBtn.addEventListener('click', addPoint);
+  removeBtn.addEventListener('click', removePoint);
   button.addEventListener('click', generateJoke);
 }
 
@@ -25,6 +31,20 @@ const generateJoke = async () => {
 function printHtml(jokeText: string) {
   let div = document.querySelector('.jokebox') as HTMLDivElement;
   div.innerHTML = jokeText;
+}
+
+function addPoint() {
+  const pointCounter = document.querySelector('#counter') as HTMLParagraphElement;
+  points += 1;
+  pointCounter.innerText = points.toString();
+  console.log(points);
+}
+
+function removePoint() {
+  const pointCounter = document.querySelector('#counter') as HTMLParagraphElement;
+  points -= 1;
+  pointCounter.innerText = points.toString();
+  console.log(points);
 }
 
 init();
